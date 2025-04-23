d34be27bc17d941dc8733c62d3b226339fba76d7
Santinorua <santinoruani@gmail.com>
2025-04-23 15:39:07 -0300

    Ingreso clave Terminado

---
+++ Assets/Scripts/ClaveManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+
+    string clave = "tic2025";
+
+
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if (claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = "";
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
